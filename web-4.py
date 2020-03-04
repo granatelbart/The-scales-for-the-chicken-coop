9e58a8d27e22a408e56bf6848f5686077b273f7c
granatelbart <maximillian@gross-hellweg.de>
2020-03-04 22:46:44 +0000

    update

--- web-4.py
+++ web-4.py
@@ -24,6 +24,19 @@ for huhn in topics:
         client.subscribe("huhn/#")
         client.loop_start()
 
+name = "kein name"
+def on_message(client, userdata, message):
+    global name
+    name = str(message.payload.decode("utf-8"))
+topics = ["name"]
+client = mqtt.Client()
+client.on_message=on_message
+client.connect("127.0.0.1", 1883, 60)
+client.loop_start()
+for name in topics:
+        client.subscribe("name/reader")
+        client.loop_start()
+
 class myHandler(BaseHTTPRequestHandler):
         def do_GET(self):
                 global value
@@ -35,7 +48,7 @@ class myHandler(BaseHTTPRequestHandler):
                 self.wfile.write(bytes("<html><head><title>Web View Waage</title></head>", "utf-8"))
                 self.wfile.write(bytes("<body><p>Nester Uebersicht</p>", "utf-8"))
                 self.wfile.write(bytes("<p>Datum: "+str (value1data)+"</p></body>", 'utf8'))
-                self.wfile.write(bytes("<table style=background-color: #fffff height: 301px; width=397 border=2 ><tbody><tr><td style=width: 123px;>&nbsp;</td><td style=width: 126px;>&nbsp;</td><td style=width: 126px;>&nbsp;</td></tr><tr><td style=width: 123px;>Nest1</td><td style=width: 126px;>Gewicht in Gramm: "+str (value)+"</td><td style=width:126px;>Huhn</td></tr><tr><td style=width: 123px;>Nest2</td> <td style=width: 126px;>Gewicht</td> <td style=width: 126px;>Huhn</td> </tr>  <tr>  <td style=width: 123px;>Nest3</td> <td style=width: 126px;>Gewicht&nbsp;</td><td style=width: 126px;>Huhn</td> </tr></tbody></table>", 'utf8'))
+                self.wfile.write(bytes("<table style=background-color: #fffff height: 301px; width=397 border=2 ><tbody><tr><td style=width: 123px;>&nbsp;</td><td style=width: 126px;>&nbsp;</td><td style=width: 126px;>&nbsp;</td></tr><tr><td style=width: 123px;>Nest1</td><td style=width: 126px;>Gewicht in Gramm: "+str (value)+"</td><td style=width:126px;>Huhn "+str (name)+"</td></tr><tr><td style=width: 123px;>Nest2</td> <td style=width: 126px;>Gewicht</td> <td style=width: 126px;>Huhn</td> </tr>  <tr>  <td style=width: 123px;>Nest3</td> <td style=width: 126px;>Gewicht&nbsp;</td><td style=width: 126px;>Huhn</td> </tr></tbody></table>", 'utf8'))
                 return
 
 try:
