d67843b338d65580bcd6b2b3a91e0cd7e3be336d
granatelbart <maximillian@gross-hellweg.de>
2020-03-11 23:53:54 +0000

    update

--- Read.py
+++ Read.py
@@ -1,34 +1,51 @@
 #!/usr/bin/env python
-import paho.mqtt.client as mqtt
-import RPi.GPIO as GPIO
-import os
-import time
-from mfrc522 import SimpleMFRC522
 
-client = mqtt.Client()
-client.connect("127.0.0.1", 1883, 60)
-client.loop_start()
+import RPi.GPIO as GPIO
+from mfrc522 import SimpleMFRC522
+import gpiozero
+import time
+from gpiozero import DigitalOutputDevice
+#GPIO.setmode(GPIO.BOARD)
+GPIO.setmode(GPIO.BCM)
+################################################################
+
+
+class HP4067Mux:
+    def __init__(self, s0, s1, s2, s3):
+        """
+        Arguments s0 to s3 represent the address input as in the datasheet
+        """
+        self._outs = [DigitalOutputDevice(pin=pin) for pin in [s0, s1, s2, s3]]
+        self.channel(0)
+    def channel(self, value):
+        """
+        Select the muxed channel from 0 to 15
+        """
+        assert 0 <= value <= 15, "Can only mux 4 lines"
+        for i, pin in enumerate(self._outs):
+            pin.value = bool(1 << i & value)
+
+
+mymux = HP4067Mux(19,6,5,12)
+mymux.channel(0)
+
+####################################################################
 
 reader = SimpleMFRC522()
 
-#try:
-#       name= reader.read()
-#        print(name)
-#        #print(alter)
-#        #print(impfung)
-#        #client.publish("huhn/name", name)
-#finally:
-#        GPIO.cleanup()
-
-GPIO.setwarnings(False)
-#GPIO.setmode(GPIO.BCM)
-pinList = [22,24,19,21,23]
-rfidList = [1081714680121,871849641659,504025229752,275844158012,648722309022,118668796333,737282492797]
-
 while True:
-    reader = SimpleMFRC522()
-    id, name = reader.read()
-    client.publish("name/reader", name)
-    time.sleep(0.5)
-#except KeyboardInterrupt:
-#    pass  # User can end this with Ctrl+C.
+   # GPIO.cleanup()
+    try:
+        id, name = reader.read()
+ 
+        print(id)
+        print(name)
+        
+        time.sleep(2) 
+
+    except KeyboardInterrupt:
+        pass  # User can end this with Ctrl+C.
+    finally:
+        print("Cleaning...")
+        GPIO.cleanup()
+        print("Bye!")
